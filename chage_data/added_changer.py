8084512d14506c53d5cc18cd2eab7f2e6f237b5f
Roma <roman.malynovsky@gmail.com>
2023-08-21 07:34:16 +0300

    this is so fucking good

--- chage_data/added_changer.py
+++ chage_data/added_changer.py
@@ -4,63 +4,63 @@ from data_analyzer import draw_graphics
 from data_list import DataHolder
 from data_printer import prints_lists_values
 
-data = load_workbook('D:/Roman/proga/python/DuoCards/data.xlsx')
-sheet = data.active
-
-previous = 0
-current = 0
-data_sum = 0
-
-added_copy = {}
-difference = {}
-
-for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
-    if row[1].value is None:
-        break
-
-    data_sum += row[1].value
-
-    for i in range(4, min(len(row), 10)):
-        if row[i].value is None:
-            break
-        current += row[i].value
-
-    if current != 0 and previous != 0:
-        actual = current - previous
-        if data_sum != actual:
-            print(row[0].value.strftime("%d/%m"))
-            print("Previous sum:", data_sum)
-            print("Actual sum:", actual)
-            print("Difference:", data_sum - actual)
-            print("Was:", row[1].value)
-            row[1].value -= (data_sum - actual)
-            if data_sum == 8888:
-                print()
-            added_copy[row[0].value] = actual
-            difference[row[0].value] = (data_sum - actual)
-            print("New value:", row[1].value)
-            print()
-
-    if current != 0:
-        previous = current
-        data_sum = 0
+def change_sheet(current_sheet):
+    previous = 0
     current = 0
+    data_sum = 0
 
-data.save("Copy.xlsx")
+    added_copy = {}
+    difference = {}
 
-calendar = list(difference.keys())
-added_copy_list = list(added_copy.values())
-difference_list = list(difference.values())
+    for row in current_sheet.iter_rows(min_row=2, max_row=current_sheet.max_row):
+        if row[1].value is None:
+            break
 
-ratio_list = [added_copy_list[i] / difference_list[i] for i in range(len(difference))]
-data_printer = DataHolder(
-    calendar,
-    "Copy",
-    (ratio_list, "ratio_list")
-)
-prints_lists_values(data_printer)
-try:
-    draw_graphics(data_printer, float(input("Write down pause time: ")))
-except:
-    draw_graphics(data_printer, 0.01)
-#%%
+        data_sum += row[1].value
+
+        for i in range(4, min(len(row), 10)):
+            if row[i].value is None:
+                break
+            current += row[i].value
+
+        if current != 0 and previous != 0:
+            actual = current - previous
+            if data_sum != actual:
+                print(row[0].value.strftime("%d/%m"))
+                print("Previous sum:", data_sum)
+                print("Actual sum:", actual)
+                print("Difference:", data_sum - actual)
+                print("Was:", row[1].value)
+                row[1].value -= (data_sum - actual)
+                added_copy[row[0].value] = actual
+                difference[row[0].value] = (data_sum - actual)
+                print("New value:", row[1].value)
+                print()
+
+        if current != 0:
+            previous = current
+            data_sum = 0
+        current = 0
+
+    data.save("Copy.xlsx")
+
+    calendar = list(difference.keys())
+    added_copy_list = list(added_copy.values())
+    difference_list = list(difference.values())
+
+    ratio_list = [added_copy_list[i] / difference_list[i] for i in range(len(difference))]
+    data_printer = DataHolder(
+        calendar,
+        "Copy",
+        (ratio_list, "ratio_list")
+    )
+    prints_lists_values(data_printer)
+    try:
+        draw_graphics(data_printer, float(input("Write down pause time: ")))
+    except:
+        draw_graphics(data_printer, 0.01)
+
+data = load_workbook('D:/Roman/proga/python/DuoCards/data.xlsx')
+sheet = data['english']
+change_sheet(sheet)
+
